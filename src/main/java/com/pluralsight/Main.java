d36c23f7532b0143d0165162ea2b1b9db5c0f885
michalhagos <mhayele3@gmail.com>
2026-05-11 15:21:38 -0500

    Added Main class and prompt user for number of players using a scanner

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,5 +1,34 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner theScanner = new Scanner(System.in);
+
+        // ask the user how many players will be playing
+        System.out.print("How many players are playing? ");
+        int numberOfPlayers = Integer.parseInt(theScanner.nextLine());
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
 
 }
