cc59d602a2531fd42c9f19e95b635432e67658db
michalhagos <mhayele3@gmail.com>
2026-05-11 14:57:14 -0500

    Add private playerName and cards fields to Hand class

--- src/main/java/com/pluralsight/Hand.java
+++ src/main/java/com/pluralsight/Hand.java
@@ -1,4 +1,15 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class Hand {
+
+    // the player's name
+    private String playerName;
+
+    // the list of cards in the player's hand
+    private ArrayList<Card> cards;
+
+
+
 }
