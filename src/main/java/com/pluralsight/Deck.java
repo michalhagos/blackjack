6188c25147b101253fb41d6b7a576ca9a6308c35
michalhagos <mhayele3@gmail.com>
2026-05-11 14:50:00 -0500

    Added constructor to Deck class that builds all 52 cards

--- src/main/java/com/pluralsight/Deck.java
+++ src/main/java/com/pluralsight/Deck.java
@@ -7,7 +7,24 @@ public class Deck {
     // the list of cards in the deck
     private ArrayList<Card> cards;
 
+    // constructor builds a full deck of 52 cards
+// loops through all suits and values to create every card
+    public Deck() {
+        cards = new ArrayList<>();
 
+        // all four suits in a standard deck
+        String[] suits = {"Hearts", "Spades", "Diamonds", "Clubs"};
+
+        // all thirteen values in a standard deck
+        String[] values = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+
+        // nested loop creates one card for every suit and value combination
+        for (String suit : suits) {
+            for (String value : values) {
+                cards.add(new Card(suit, value));
+            }
+        }
+    }
 
 
 }
