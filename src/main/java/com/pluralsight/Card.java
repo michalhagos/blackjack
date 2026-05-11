fe7ccb65bc34b995fee220d34dcda1b3a1452cd7
michalhagos <mhayele3@gmail.com>
2026-05-11 14:28:45 -0500

    Created the classes I need started working on the Card class

---
+++ src/main/java/com/pluralsight/Card.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class Card {
+    // the suit of the card like the Hearts, Spades, Diamonds, Clubs
+    private String suit;
+    // the value of the card - 2, 3, 4 ... 10, J, Q, K, A
+    private String value;
+    // tracks whether the card is face up or face down
+    private boolean isFaceUp;
+
+
+
+
+
+
+}
